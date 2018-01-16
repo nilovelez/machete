5bf7c118a3790f04d4f02be49cd47b65f9e5c9ba
Nilo Velez <nilovelez@gmail.com>
2018-01-16 21:17:28 +0100

    updated description

--- machete.php
+++ machete.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Machete
 Plugin URI: https://machetewp.com
-Description: Machete is a lean and simple suite of tools that solve common WordPress anoyances: cookie bar, tracking codes, header cleanup
+Description: Machete is a lean and simple suite of tools that makes WordPress development easier: cookie bar, tracking codes, custom code editor, header cleanup, post and page cloner
 Version: 3.0.4
 Author: Nilo Velez
-Author URI: http://www.nilovelez.com
+Author URI: https://www.nilovelez.com
 License: WTFPL
 License URI: http://www.wtfpl.net/txt/copying/
 
