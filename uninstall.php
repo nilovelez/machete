93053fc70700b8c86f4ce1c252dc11fde684654b
Nilo Velez <nilovelez@gmail.com>
2018-01-16 21:42:55 +0100

    updated post-uninstall script

--- uninstall.php
+++ uninstall.php
@@ -12,12 +12,17 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit;
 
+// machete module manager options:
+delete_option('machete_disabled_modules');
+
 // machete cleanup options:
 delete_option('machete_cleanup_settings');
 
 // machete utils options:
 delete_option('machete_utils_settings');
 
-
 // machete cookie bar options:
 delete_option('machete_cookies_settings');
+
+// machete maintenance mode options:
+delete_option('machete_maintenance_settings');
