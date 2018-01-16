548d6d76f0f4de2bac040fd71be0e1fc8b91b248
Nilo Velez <nilovelez@gmail.com>
2018-01-17 00:07:14 +0100

    new structure to allow external modules ¡Viva la revolución!

--- machete.php
+++ machete.php
@@ -33,142 +33,47 @@ function machete_load_plugin_textdomain() {
     load_plugin_textdomain( 'machete', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 
-class machete {
-	public $modules = array();
+// include main Machete classes
+require ('inc/class-machete.php');
+require ('inc/class-machete-module.php');
 
-	public function __construct(){
-		add_action('init',array($this,'init'));
-	}
-
-	public function init(){
-		global $machete;
-		
-		require ('inc/machete_modules.php');
-
-		if ( ! is_admin() ) {
-			define('MACHETE_FRONT_INIT',true);
-			require_once('machete_frontend.php');
-		} else {
-			define('MACHETE_ADMIN_INIT',true);
-			require_once('machete_admin.php');	
-		}
-	}
-
-	function admin_tabs($current = '') {
-				
-		$is_admin = current_user_can('manage_options') ? true : false;
-
-		echo '<h2 class="nav-tab-wrapper">';
-		foreach($this->modules as $module) {
-
-			$params = $module->params;
-
-			if (!$is_admin && ($params['role'] == 'manage_options')) continue;
-			
-			if ( ! $params['is_active'] ) continue;
-			if ( ! $params['has_config'] ) continue;
-
-			$slug = 'machete-'.$params['slug'];
-			if ($slug == $current){
-				echo '<a href="#" class="nav-tab-active nav-tab '.$slug.'-tab">'.$params['title'].'</a>';
-			}else{
-				echo '<a href="'.admin_url('admin.php?page='.$slug).'" class="nav-tab '.$slug.'-tab">'.$params['title'].'</a>';
-			}
-		}
-		echo '</h2>';
-	}
-
-	public $notice_message;
-	public $notice_class;
-	public function notice( $message, $level = 'info', $dismissible = true) {
-
-		$this->notice_message = $message;
-
-		if (!in_array($level, array('error','warning','info','success'))){
-			$level = 'info';
-		}
-		$this->notice_class = 'notice notice-'.$level;
-		if ($dismissible){
-			$this->notice_class .= ' is-dismissible';
-		}
-		add_action( 'admin_notices', array( $this, 'display_notice' ) );
-	}
-				
-	public function display_notice() {
-		if (!empty($this->notice_message)){
-		?>
-		<div class="<?php echo $this->notice_class ?>">
-			<p><?php echo $this->notice_message; ?></p>
-		</div>
-		<?php }
-	}
-
-	public function manage_modules ($module, $action, $silent = false){
-		
-		if (empty($module) || empty($action) || in_array($action, array('enable','disable'))) {
-			if (!$silent) $this->notice(__( 'Bad request', 'machete' ), 'error');
-			return false;
-		}
-
-		if ( ! array_key_exists( $module, $this->modules)) {
-			if (!$silent) $this->notice(__( 'Uknown module:', 'machete' ) . ' ' . $module, 'error');
-			return false;
-		}
-
-		if(! $disabled_modules = get_option('machete_disabled_modules')){
-			$disabled_modules = array();
-		}
-		
-		if ($action == 'deactivate') {
-			if(in_array($module, $disabled_modules)){
-				if (!$silent) $this->notice(__( 'Nothing to do. The module was already disabled.', 'machete' ), 'notice');
-				return false;
-			}
-			if ( ! $this->modules[$module]->params['can_be_disabled'] ) {
-				if (!$silent) $this->notice(__( 'Sorry, you can\'t disable that module', 'machete' ), 'warning');
-				return false;
-			} 
-
-			$disabled_modules[] = $module;
-
-			if (update_option('machete_disabled_modules',$disabled_modules)){
-				$this->modules[$module]->params['is_active'] = false;
-				if (!$silent) $this->notice(sprintf(__('Module %s disabled succesfully', 'machete'),
-					$this->modules[$module]->params['title'] ), 'success');
-				return true;
-
-			}else{
-				if (!$silent) $this->notice(__( 'Error saving configuration to database.', 'machete' ), 'error');;
-				return false;
-			}
-
-
-		}
-
-		if ($action == 'activate') {
-			if($this->modules[$module]->params['is_active']){
-				if (!$silent) $this->notice(__( 'Nothing to do. The module was already active.', 'machete' ), 'notice');
-				return false;
-			}
-			if ( $module == 'powertools' ) {
-				if (!$silent) $this->notice(__( 'Sorry, you can\'t enable that module', 'machete' ), 'warning');
-				return false;
-			} 
-
-			$disabled_modules = array_diff($disabled_modules, array($module));
-
-			if (update_option('machete_disabled_modules',$disabled_modules)){
-				$this->modules[$module]->params['is_active'] = true;
-				if (!$silent) $this->notice(sprintf(__('Module %s enabled succesfully', 'machete'),
-					$this->modules[$module]->params['title'] ), 'success');
-				return true;
-
-			}else{
-				if (!$silent) $this->notice(__( 'Error saving configuration to database.', 'machete' ), 'error');;
-				return false;
-			}
-		}
-	}
-
-}
+// include Machete modules
 $machete = new machete();
+require ('inc/about/module.php');
+require ('inc/cleanup/module.php');
+require ('inc/cookies/module.php');
+require ('inc/utils/module.php');
+require ('inc/maintenance/module.php');
+require ('inc/clone/module.php');
+require ('inc/importexport/module.php');
+require ('inc/powertools/module.php');
+
+// manage disabled modules
+if($machete_disabled_modules = get_option('machete_disabled_modules')){
+	foreach ($machete_disabled_modules as $module) {
+		
+		if (isset($machete->modules[$module]) && $machete->modules[$module]->params['can_be_disabled']){
+			$machete->modules[$module]->params['is_active'] = false;
+		}
+	}
+}
+
+// manage external modules
+if (defined('MACHETE_POWERTOOLS_INIT')) {
+	$machete->modules['powertools']->params['is_active'] = true;
+	$machete->modules['powertools']->params['description'] = __('Machete PowerTools are now active! Enjoy your new toy!','machete');
+}
+
+// main init
+add_action('init', function(){
+
+	global $machete;
+
+	if ( ! is_admin() ) {
+		define('MACHETE_FRONT_INIT',true);
+		require_once('machete_frontend.php');
+	} else {
+		define('MACHETE_ADMIN_INIT',true);
+		require_once('machete_admin.php');	
+	}
+});
