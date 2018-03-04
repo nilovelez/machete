c7d820523bcbdb4f9c9cb57bd6ddb857639f7bbe
Nilo Velez <nilovelez@gmail.com>
2018-03-04 20:33:15 +0100

    Refactor to adhere to WordPress coding standars

--- inc/about/templates/machete_module.php
+++ inc/about/templates/machete_module.php
@@ -1,90 +1,97 @@
-<div class="machete-module-wrap"><div class="machete-module <?php echo $slug.'-module' ?> module-is-<?php echo $args['is_active'] ? 'active' : 'inactive' ?>">
-				
-				<?php if ($args['is_active'] && $args['has_config']) { ?>
+<?php
+	$args['tab_url'] = add_query_arg( 'page', 'machete-' . $slug, admin_url( 'admin.php' ) );
+	$args['img_url'] = MACHETE_BASE_URL . 'inc/' . $slug . '/banner.svg';
+?>
+<div class="machete-module-wrap"><div class="machete-module <?php echo esc_attr( $slug . '-module' ); ?> module-is-<?php echo $args['is_active'] ? 'active' : 'inactive'; ?>">
 
-					<a class="machete-module-image"
-						href="<?php echo admin_url('admin.php?page=machete-'.$slug) ?>"
-						title="<?php echo __('Configure','machete').' '.$args['full_title'] ?>">
-						<img src="<?php echo MACHETE_BASE_URL .'inc/'. $slug ?>/banner.svg">
-					</a>
+<?php if ( $args['is_active'] && $args['has_config'] ) { ?>
+	<a class="machete-module-image"
+		href="<?php echo esc_url( $args['tab_url'] ); ?>"
+		title="<?php echo esc_attr( __( 'Configure', 'machete' ) . ' ' . $args['full_title'] ); ?>">
+		<img src="<?php echo esc_url( $args['img_url'] ); ?>">
+	</a>
+<?php } else { ?>
+	<span class="machete-module-image" title="<?php echo esc_attr( $args['full_title'] ); ?>">
+		<img src="<?php echo esc_url( $args['img_url'] ); ?>">
+	</span>
+<?php } ?>
 
-				<?php } else { ?>
+	<div class="machete-module-text">
+		<?php if ( $args['has_config'] ) { ?>
+			<h3><a href="<?php echo esc_url( $args['tab_url'] ); ?>"
+		title="<?php echo esc_attr( __( 'Configure', 'machete' ) . ' ' . $args['full_title'] ); ?>"><?php echo esc_html( $args['full_title'] ); ?></a></h3>
+		<?php } else { ?>
+			<h3><?php echo esc_html( $args['full_title'] ); ?></h3>
+		<?php } ?>
 
-					<span class="machete-module-image" title="<?php echo $args['full_title'] ?>">
-						<img src="<?php echo MACHETE_BASE_URL . 'inc/' . $slug ?>/banner.svg">
-					</span>
+		<div class="machete-module-description"><?php echo esc_html( $args['description'] ); ?></div>
 
-				<?php } ?>
+		<?php if ( $args['is_active'] ) { ?>
+		<div class="machete-module-active-indicator"><?php esc_html_e( 'Active', 'machete' ); ?></div>
+		<?php } ?>
+	</div>
 
-				<div class="machete-module-text">
-					<?php if ($args['has_config']) { ?>
-						<h3><a href="<?php echo admin_url('admin.php?page=machete-'.$slug) ?>"
-						title="<?php echo __('Configure','machete').' '.$args['full_title'] ?>"><?php echo $args['full_title'] ?></a></h3>
+	<div class="machete-module-bottom">
+		<div class="machete-module-toggle-active">
+		<?php
 
-					<?php } else { ?>
-						<h3><?php echo $args['full_title'] ?></h3>
+		$action_url = wp_nonce_url( add_query_arg( array(
+			'page'   => 'machete',
+			'module' => $slug,
+		), admin_url( 'admin.php' ) ), 'machete_action_' . $slug );
 
-					<?php } ?>
+		if ( 'powertools' === $slug ) {
 
-					<div class="machete-module-description">
-						<?php echo $args['description'] ?>
-					</div>
-					<?php if ($args['is_active']){ ?><div class="machete-module-active-indicator"><?php _e('Active','machete') ?></div><?php } ?>
+			$machete_powertools_path = 'machete-powertools/machete-powertools.php';
 
-				</div>
-				<div class="machete-module-bottom">
-					<div class="machete-module-toggle-active">
-					<?php 
+			if ( $args['is_active'] ) {
 
-					$action_url = wp_nonce_url( admin_url( "admin.php?page=machete&amp;module=" . $slug), 'machete_action_' . $slug );
+				$machete_powertools_deactivation_url = wp_nonce_url( add_query_arg( array(
+					'action' => 'deactivate',
+					'plugin' => $machete_powertools_path,
+				), admin_url( 'plugins.php' ) ), 'deactivate-plugin_' . $machete_powertools_path );
+				?>
 
-					if ('powertools' == $slug) { 
+				<a href="<?php echo esc_url( $machete_powertools_deactivation_url ); ?>" class="button-secondary" data-status="1"><?php esc_html_e( 'Deactivate Plugin', 'machete' ); ?></a>
 
-						$machete_powertools_path = 'machete-powertools/machete-powertools.php';
+				<a href="<?php echo esc_url( add_query_arg( 'page', 'machete-' . $slug, admin_url( 'admin.php' ) ) ); ?>"
+				title="<?php echo esc_attr( __( 'Configure', 'machete' ) . ' ' . $args['full_title'] ); ?>" class="button-secondary" data-status="0"><?php esc_html_e( 'Settings', 'machete' ); ?></a>
 
-						if ($args['is_active']) {
+			<?php
+			} elseif ( file_exists( WP_PLUGIN_DIR . '/' . $machete_powertools_path ) ) {
 
-							$machete_powertools_deactivation_url = wp_nonce_url(admin_url('plugins.php?action=deactivate&plugin='.$machete_powertools_path), 'deactivate-plugin_'.$machete_powertools_path);
-							?>
+				$machete_powertools_activation_url = wp_nonce_url( add_query_arg( array(
+					'action' => 'activate',
+					'plugin' => $machete_powertools_path,
+				), admin_url( 'plugins.php' ) ), 'activate-plugin_' . $machete_powertools_path );
+				?>
 
-							<a href="<?php echo $machete_powertools_deactivation_url ?>" class="button-secondary" data-status="1"><?php _e('Deactivate Plugin','machete') ?></a>
+				<a href="<?php echo esc_url( $machete_powertools_activation_url ); ?>" class="button-primary" data-status="1"><?php esc_html_e( 'Activate Plugin', 'machete' ); ?></a>
 
-							<a href="<?php echo admin_url('admin.php?page=machete-'.$slug) ?>"
-						title="<?php echo __('Configure','machete').' '.$args['full_title'] ?>" class="button-secondary" data-status="0"><?php _e('Settings','machete') ?></a>
-						<?php } elseif (file_exists(WP_PLUGIN_DIR.'/'.$machete_powertools_path)) { 
+			<?php } else { ?>
 
-							
-							$machete_powertools_activation_url = wp_nonce_url(admin_url('plugins.php?action=activate&plugin='.$machete_powertools_path), 'activate-plugin_'.$machete_powertools_path);
+				<a href="https://machetewp.com/powertools/" class="button-primary" data-status="1"><?php esc_html_e( 'Download Machete PowerTools', 'machete' ); ?></a>
 
-							?>
-
-							<a href="<?php echo $machete_powertools_activation_url ?>" class="button-primary" data-status="1"><?php _e('Activate Plugin','machete') ?></a>
-
-						<?php } else { ?>
-							<a href="https://machetewp.com/powertools/" class="button-primary" data-status="1"><?php _e('Download Machete PowerTools','machete') ?></a>
-
-						<?php } ?>
-
-
-					<?php } else if ($args['is_active']) { ?>
-
-						<?php if ($args['can_be_disabled']) { ?>
-						<a href="<?php echo $action_url.'&amp;machete-action=deactivate' ?>" class="button-secondary" data-status="0"><?php _e('Deactivate','machete') ?></a>
-						<?php } ?>
-
-						<?php if ($args['has_config']) { ?>
-						<a href="<?php echo admin_url('admin.php?page=machete-'.$slug) ?>"
-						title="<?php echo __('Configure','machete').' '.$args['full_title'] ?>" class="button-secondary" data-status="0"><?php _e('Settings','machete') ?></a>
-						<?php } ?>
-
-					<?php } else { ?>
-
-						<a href="<?php echo $action_url.'&amp;machete-action=activate' ?>" class="button-secondary" data-status="1"><?php _e('Activate','machete') ?></a>
-
-					<?php } ?>
-					</div>
-
-				</div>
+			<?php } ?>
 
 
-			</div></div>
+		<?php } elseif ( $args['is_active'] ) { ?>
+
+			<?php if ( $args['can_be_disabled'] ) { ?>
+			<a href="<?php echo esc_url( add_query_arg( 'machete-action', 'deactivate', $action_url ) ); ?>" class="button-secondary" data-status="0"><?php esc_html_e( 'Deactivate', 'machete' ); ?></a>
+			<?php } ?>
+
+			<?php if ( $args['has_config'] ) { ?>
+			<a href="<?php echo esc_url( add_query_arg( 'page', 'machete-' . $slug, admin_url( 'admin.php' ) ) ); ?>"
+			title="<?php echo esc_attr( __( 'Configure', 'machete' ) . ' ' . $args['full_title'] ); ?>" class="button-secondary" data-status="0"><?php esc_html_e( 'Settings', 'machete' ); ?></a>
+			<?php } ?>
+
+		<?php } else { ?>
+
+			<a href="<?php echo esc_url( add_query_arg( 'machete-action', 'activate', $action_url ) ); ?>" class="button-secondary" data-status="1"><?php esc_html_e( 'Activate', 'machete' ); ?></a>
+
+		<?php } ?>
+		</div>
+	</div>
+
+</div></div>
