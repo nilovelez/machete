a1065b3705dff511295eab45a68168bf5833e965
Nilo Velez <nilovelez@gmail.com>
2018-03-07 00:50:01 +0100

    Refactor to adhere to WordPress coding standars

--- inc/cookies/admin-content.php
+++ inc/cookies/admin-content.php
@@ -1,14 +1,15 @@
-<?php if ( ! defined( 'MACHETE_ADMIN_INIT' ) ) exit; ?>
+<?php if ( ! defined( 'MACHETE_ADMIN_INIT' ) ) {
+	exit;
+}
+?>
 
 <div class="wrap machete-wrap machete-section-wrap">
-	<h1><?php _e('Cookie Law Warning','machete') ?></h1>
-	<p class="tab-description"><?php _e('We know you hate cookie warning bars. Well, this is the less hateable cookie bar you\'ll find. It is really light, it won\'t affect your PageSpeed score and plays well with static cache plugins.','machete') ?></p>
+	<h1><?php esc_html_e( 'Cookie Law Warning', 'machete' ); ?></h1>
+	<p class="tab-description"><?php esc_html_e( 'We know you hate cookie warning bars. Well, this is the less hateable cookie bar you\'ll find. It is really light, it won\'t affect your PageSpeed score and plays well with static cache plugins.', 'machete' ); ?></p>
 
-	<?php $machete->admin_tabs('machete-cookies'); ?>
+	<?php $machete->admin_tabs( 'machete-cookies' ); ?>
 
-	<p class="tab-performance"><span><strong><i class="dashicons dashicons-clock"></i> <?php _e('Performance impact:','machete') ?></strong> <?php _e('This tool adds 0,4Kb and a single database request request to each page load. The remaining <abbr title="~1.1Kb if GZipped">~2.5Kb</abbr> of code is loaded asynchronously via javascript from a pregenerated static file.','machete') ?></span></p>
-
-
+	<p class="tab-performance"><span><strong><i class="dashicons dashicons-clock"></i> <?php esc_html_e( 'Performance impact:', 'machete' ); ?></strong> <?php echo wp_kses( _e( 'This tool adds 0,4Kb and a single database request request to each page load. The remaining <abbr title="~1.1Kb if GZipped">~2.5Kb</abbr> of code is loaded asynchronously via javascript from a pregenerated static file.', 'machete' ), wp_kses_allowed_html() ); ?></span></p>
 
 <form id="mache-cookies-options" action="" method="POST">
 
@@ -21,28 +22,27 @@
 <tr>
 
 <tr>
-<th scope="row"><?php _e('Cookie alert status','machete') ?></th>
-<td><fieldset><legend class="screen-reader-text"><span><?php _e('Cookie alert status','machete') ?></span></legend>
-	<label><input name="bar_status" value="enabled" type="radio" <?php if ($this->settings['bar_status'] =='enabled') echo 'checked="checked"'; ?>> <?php _e('Enabled','machete') ?></label><br>
-	<label><input name="bar_status" value="disabled" type="radio" <?php if ($this->settings['bar_status'] =='disabled') echo 'checked="checked"'; ?>> <?php _e('Disabled','machete') ?></label><br>
-	
+<th scope="row"><?php esc_html_e( 'Cookie alert status', 'machete' ); ?></th>
+<td><fieldset><legend class="screen-reader-text"><span><?php esc_html_e( 'Cookie alert status', 'machete' ); ?></span></legend>
+	<label><input name="bar_status" value="enabled" type="radio" <?php if ( $this->settings['bar_status'] =='enabled' ) echo 'checked="checked"'; ?>> <?php esc_html_e( 'Enabled', 'machete' ); ?></label><br>
+	<label><input name="bar_status" value="disabled" type="radio" <?php if ($this->settings['bar_status'] =='disabled') echo 'checked="checked"'; ?>> <?php esc_html_e( 'Disabled', 'machete' ); ?></label><br>
 </fieldset></td>
 </tr>
 
 <tr>
-<th scope="row"><label for="warning_text"><?php _e('Cookie warning text','machete') ?></label></th>
-<td><textarea name="warning_text" rows="3" cols="50" id="warning_text" class="large-text code"><?php echo esc_textarea(stripslashes($this->settings['warning_text'])) ?></textarea>
-<p class="description" style="text-align: right;"><a id="restore_cookie_text_btn"><?php _e('Restore default warning text','machete') ?></a></p>
+<th scope="row"><label for="warning_text"><?php esc_html_e( 'Cookie warning text', 'machete' ); ?></label></th>
+<td><textarea name="warning_text" rows="3" cols="50" id="warning_text" class="large-text code"><?php echo esc_textarea( stripslashes( $this->settings['warning_text'] ) ); ?></textarea>
+<p class="description" style="text-align: right;"><a id="restore_cookie_text_btn"><?php esc_html_e( 'Restore default warning text', 'machete'); ?></a></p>
 </fieldset></td>
 </tr>
 <tr>
-<th scope="row"><label for="accept_text"><?php _e('Accept button text','machete') ?></label></th>
-<td><input name="accept_text" id="accept_text" value="<?php echo esc_html($this->settings['accept_text']) ?>" class="regular-text ltr" type="text"></td>
+<th scope="row"><label for="accept_text"><?php esc_html_e( 'Accept button text', 'machete' ); ?></label></th>
+<td><input name="accept_text" id="accept_text" value="<?php echo esc_html( $this->settings['accept_text'] ); ?>" class="regular-text ltr" type="text"></td>
 </tr>
 
 <tr>
-<th scope="row"><?php _e('Cookie bar theme','machete') ?></th>
-<td><fieldset><legend class="screen-reader-text"><span><?php _e('Cookie bar theme','machete') ?></span></legend>
+<th scope="row"><?php esc_html_e( 'Cookie bar theme', 'machete' ) ?></th>
+<td><fieldset><legend class="screen-reader-text"><span><?php esc_html_e( 'Cookie bar theme', 'machete' ); ?></span></legend>
 	<label><input name="bar_theme" class="bar-theme-radio" value="light" type="radio" <?php if ($this->settings['bar_theme'] =='light') echo 'checked="checked"'; ?>> <?php _e('Classic Light','machete') ?></label><br>
 	<label><input name="bar_theme" class="bar-theme-radio" value="dark" type="radio" <?php if ($this->settings['bar_theme'] =='dark') echo 'checked="checked"'; ?>> <?php _e('Classic Dark','machete') ?></label><br>
 	<label><input name="bar_theme" class="bar-theme-radio" value="new_light" type="radio" <?php if ($this->settings['bar_theme'] =='new_light') echo 'checked="checked"'; ?>> <?php _e('Modern Light','machete') ?></label><br>
@@ -61,23 +61,23 @@
 
 <div class="postbox machete-helpbox" data-collapsed="false">
 <button type="button" class="handlediv button-link"><span class="toggle-indicator" aria-hidden="true"><span class="dashicons dashicons-arrow-up"></span></span></button>
-	<h3 class="hndle"><span><span class="dashicons dashicons-sos"></span> <?php echo _e('How do I customize the cookie bar?','machete') ?></span></h2>
+	<h3 class="hndle"><span><span class="dashicons dashicons-sos"></span> <?php esc_html_e( 'How do I customize the cookie bar?', 'machete' ) ?></span></h2>
 	<div class="inside">
 		<table class="form-table"><tbody><tr valign="top"><th scope="row"></th><td>
 
-			<p class="description"><?php printf(__('You can customize the cookie bar CSS using the %sAnalytics & Code tab','machete'),
-            '<a href="'.admin_url('admin.php?page=machete-utils').'">') ?></a></p>
-            <p class="description"><?php _e('For your reference, this is the HTML used to render the cookie bar:','machete') ?></p>
-            <pre style="color: #00f; font-weight: bold;">&lt;div <span style="color: #c00;">id</span>=<span style="color: #f0f;">"machete_cookie_container"</span> <span style="color: #c00;">class</span>=<span style="color: #f0f;">"machete_cookie_container"</span>&gt;
-  &lt;div <span style="color: #c00;">id</span>=<span style="color: #f0f;">"machete_cookie_bar"</span> <span style="color: #c00;">class</span>=<span style="color: #f0f;">"machete_cookie_bar"</span>&gt;
-    &lt;a <span style="color: #c00;">id</span>=<span style="color: #f0f;">"machete_accept_cookie_btn"</span> <span style="color: #c00;">class</span>=<span style="color: #f0f;">"machete_accept_cookie_btn"</span>&gt;
-      <span style="color: #000;">[<?php _e('Accept button text','machete') ?>]</span>
-    &lt;/a&gt;
-    <span style="color: #000;">[<?php _e('Cookie warning text','machete') ?>]</span>
-  &lt;/div&gt;
+			<p class="description"><?php echo printf( __( 'You can customize the cookie bar CSS using the %sAnalytics & Code tab', 'machete' ),
+			'<a href="' . admin_url( 'admin.php?page=machete-utils' ) . '">' ); ?></a></p>
+			<p class="description"><?php esc_html_e( 'For your reference, this is the HTML used to render the cookie bar:', 'machete' ); ?></p>
+			<pre style="color: #00f; font-weight: bold;">&lt;div <span style="color: #c00;">id</span>=<span style="color: #f0f;">"machete_cookie_container"</span> <span style="color: #c00;">class</span>=<span style="color: #f0f;">"machete_cookie_container"</span>&gt;
+&nbsp;&nbsp;&lt;div <span style="color: #c00;">id</span>=<span style="color: #f0f;">"machete_cookie_bar"</span> <span style="color: #c00;">class</span>=<span style="color: #f0f;">"machete_cookie_bar"</span>&gt;
+&nbsp;&nbsp;&nbsp;&nbsp;&lt;a <span style="color: #c00;">id</span>=<span style="color: #f0f;">"machete_accept_cookie_btn"</span> <span style="color: #c00;">class</span>=<span style="color: #f0f;">"machete_accept_cookie_btn"</span>&gt;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color: #000;">[<?php esc_html_e( 'Accept button text', 'machete' ); ?>]</span>
+&nbsp;&nbsp;&nbsp;&nbsp;&lt;/a&gt;
+&nbsp;&nbsp;&nbsp;&nbsp;<span style="color: #000;">[<?php esc_html_e( 'Cookie warning text', 'machete' ); ?>]</span>
+&nbsp;&nbsp;&lt;/div&gt;
 &lt;/div&gt;</pre>
 		</td></tr></tbody></table> 
-    </div>
+	</div>
 </div>
 
 </div>
@@ -86,34 +86,34 @@
 <script>
 
 	var cookie_bar_templates = [];
-	<?php 
+	<?php
 	$machete_cookie_replaces = array(
 		'{{accept_text}}'  => '<span id="machete_cookie_preview_accept"></span>',
-		'{{warning_text}}' => '<span id="machete_cookie_preview_warning"></span>'
+		'{{warning_text}}' => '<span id="machete_cookie_preview_warning"></span>',
 	);
 
-	foreach ($this->themes as $slug => $params) {
-		$params['html'] = file_get_contents($params['template']);
+	foreach ( $this->themes as $slug => $params ) {
+		$params['html'] = file_get_content( $params['template'] );
 
-		$machete_cookie_replaces['{{extra_css}}'] = $this->themes[$slug]['extra_css'];
+		$machete_cookie_replaces['{{extra_css}}'] = $this->themes[ $slug ]['extra_css'];
 
 		$params['html'] = str_replace(
-			array_keys($machete_cookie_replaces),
-			array_values($machete_cookie_replaces),
+			array_keys( $machete_cookie_replaces ),
+			array_values( $machete_cookie_replaces ),
 			$params['html']
 		);
 	?>
-	cookie_bar_templates['<?php echo $slug ?>'] = '<?php echo addslashes($params['html']) ?>';
+	cookie_bar_templates['<?php echo $slug; ?>'] = '<?php echo addslashes( $params['html'] ); ?>';
 
 	<?php } ?>
 
 (function($){
 
 	$(".bar-theme-radio").change( function(){
-	   if( $(this).is(':checked') ){
-	    //alert( $(this).val() );
-	    render_bar_preview($(this).val());
-	   }
+		if( $(this).is(':checked') ){
+			//alert( $(this).val() );
+			render_bar_preview($(this).val());
+		}
 	});
 
 	$("#warning_text").on('input', function(){
@@ -140,7 +140,7 @@
     container.id = 'machete_cookie_container';
     container.className = 'machete_cookie_container';
     container.innerHTML = cookie_bar_templates['<?php echo $this->settings['bar_theme'] ?>'];
-	
+
     Object.assign(container.style,{
         position: 'fixed',
         zIndex: 99999,
@@ -150,7 +150,7 @@
     var body = document.getElementsByTagName('body')[0];
     body.appendChild(container);
     update_preview_text();
-	
+
 
 })(jQuery);
 </script>
@@ -160,10 +160,9 @@
 
 (function($){
 
-	
 
 	$('#mache-cookies-options').submit(function( e ) {
-		
+
 		if ( $( '#warning_text' ).val() == '' ) {
 			window.alert('<?php echo esc_js(__('Cookie warning text can\'t be blank', 'machete' )); ?>');
 			e.preventDefault();
@@ -196,9 +195,9 @@
 		} else {
 			open_helpbox(container);
 		}
-		
-	});
 	
+	});
+
 	var close_helpbox  = function(elem){
 		elem.attr('data-collapsed','true');
 		elem.find('div.inside').hide();
