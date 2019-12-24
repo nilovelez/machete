03cdc707e32b2438b94cce6c78065abab8d197fc
Nilo Velez <nilovelez@gmail.com>
2019-12-24 17:09:21 +0100

    new share js without jQuery

--- inc/social/js/share.js
+++ inc/social/js/share.js
@@ -1,10 +1,17 @@
-(function($) {	
-	"use strict";
-	$('.mct-share-buttons a').on('click', function( e ){
-		e.preventDefault();
-		window.open(
-			$( this ).attr('href'),
-			$( this ).attr('data-network'),
-			"toolbar=yes, top=50, left=50, width=400, height=400");
-	});
-})( jQuery );
+"use strict";
+(function() {
+	if ( ! Element.prototype.querySelectorAll ) {
+		return;
+	}
+	var elements = document.querySelectorAll('.mct-share-buttons a');
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].addEventListener('click', function( e ){
+			e.preventDefault();
+			window.open(
+				this.getAttribute("href"),
+				this.getAttribute("data-network"),
+				"toolbar=yes, top=50, left=50, width=500, height=500"
+				);
+		}, false);
+	}
+}());
