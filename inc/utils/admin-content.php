423f7ad0a585b22050ae22d87eee1a363b9e374d
Nilo Velez <nilovelez@gmail.com>
2020-02-21 00:13:08 +0100

    Some refinements to the utils module tabs

--- inc/utils/admin-content.php
+++ inc/utils/admin-content.php
@@ -138,9 +138,9 @@ if ( false !== $machete_settings ) {
 	<fieldset><legend class="screen-reader-text"><?php esc_html_e( 'Custom header content', 'machete' ); ?></legend>
 
 	<?php // translators: $s: file path. ?>
-	<p><label for="header_content"><?php printf( wp_kses_post( __( 'This code is included before the closing <code>&lt;/head&gt;</code> label.<br>Content is saved to <code>%s</code> and served using PHP\'s <code>readfile()</code> function, so no PHP or shortcodes here.', 'machete' ) ), esc_html( MACHETE_RELATIVE_DATA_PATH . 'header.html' ) ); ?></label></p>
+	<label for="header_content"><p><?php printf( wp_kses_post( __( 'This code is included before the closing <code>&lt;/head&gt;</code> label.<br>Content is saved to <code>%s</code> and served using PHP\'s <code>readfile()</code> function, so no PHP or shortcodes here.', 'machete' ) ), esc_html( MACHETE_RELATIVE_DATA_PATH . 'header.html' ) ); ?></p></label>
 
-	<p><textarea name="header_content" rows="8" cols="50" id="header_content" class="large-text code"><?php echo esc_textarea( $machete_header_content ); ?></textarea></p>
+	<textarea name="header_content" rows="8" cols="50" id="header_content" class="large-text code"><?php echo esc_textarea( $machete_header_content ); ?></textarea>
 	</fieldset>
 
 </div>
@@ -152,31 +152,40 @@ if ( false !== $machete_settings ) {
 	<h2><?php esc_html_e( 'Custom body content', 'machete' ); ?></h2>
 
 	<fieldset><legend class="screen-reader-text"><?php esc_html_e( 'Custom body content', 'machete' ); ?></legend>
-	<p><label for="alfonso_content"><?php echo wp_kses_post( __( 'This block is meant to be included just after the begining of the <code>&lt;body&gt;</code> label, and it\'s mainly used for conversion tracking codes.', 'machete' ) ); ?><br>
+	<label for="alfonso_content"><p><?php echo wp_kses_post( __( 'This block is meant to be included just after the begining of the <code>&lt;body&gt;</code> label, and it\'s mainly used for conversion tracking codes.', 'machete' ) ); ?><br>
 		<?php // translators: $s: file path. ?>
 		<?php printf( wp_kses_post( __( 'Content is saved to <code>%s</code> and served using PHP.  No PHP code or shortcodes here.', 'machete' ) ), esc_html( MACHETE_RELATIVE_DATA_PATH . 'body.html' ) ); ?>
-
-	</label></p>
-
-
+	</p></label>
 	
-
 	
-	<p>
 	<textarea name="alfonso_content" rows="8" cols="50" id="alfonso_content" class="large-text code"><?php echo esc_textarea( $machete_alfonso_content ); ?></textarea>
-	</p>
+	
 	</fieldset>
 
 
-	<fieldset>
 
-		<p class="description" style="margin-bottom: 1em;"><legend><span><?php esc_html_e( 'Custom body content injection method:', 'machete' ); ?></span></legend></p>
+<table class="form-table">
+	<tbody><tr>
+	<tr>
+	<th scope="row"><?php esc_html_e( 'Code injection method', 'machete' ); ?></th>
+	<td><fieldset>
+
+		<legend class="screen-reader-text"><span><?php esc_html_e( 'Custom body content injection method:', 'machete' ); ?></span></legend>
 		
 		<label><input name="alfonso_content_injection_method" value="wp_body" type="radio" <?php checked( 'wp_body', $this->settings['alfonso_content_injection_method'], true ); ?>> <?php echo wp_kses_data( __( 'Include the code using the native <code>wp_body</code> hook (not supported by all themes).') ); ?></label><br>
+
 		<?php // translators: %s url of article with explanation. ?> 
 		<label><input name="alfonso_content_injection_method" value="auto" type="radio" <?php checked( 'auto', $this->settings['alfonso_content_injection_method'], true ); ?>> <?php printf( wp_kses_data( __( 'Try to inject the code automatically using <a href="%s" target="_blank" rel="nofollow">Yaniv Friedensohn\'s method</a>', 'machete' ) ), 'http://www.affectivia.com/blog/placing-the-google-tag-manager-in-wordpress-after-the-body-tag/' ); ?></label><br>
+
 		<label><input name="alfonso_content_injection_method" value="manual" type="radio" <?php checked( 'manual', $this->settings['alfonso_content_injection_method'], true ); ?>> <?php echo wp_kses_data( __( 'Edit your theme\'s <code>header.php</code> template manually and include this function:', 'machete' ) ); ?> <code>&lt;?php machete_custom_body_content() ?&gt;</code></label>
-	</fieldset>
+	</fieldset></td>
+	</tr>
+	</table>
+
+
+
+
+
 
 
 
@@ -189,15 +198,12 @@ if ( false !== $machete_settings ) {
 
 	<fieldset><legend class="screen-reader-text"><span><?php esc_html_e( 'Custom footer content', 'machete' ); ?></span></legend>
 	<?php // translators: $s: file path. ?>
-	<p><label for="footer_content"><?php printf( wp_kses_post( __( 'This code is included when the <code>wp_footer</code> action is called, normally just before the closing <code>&lt;/body&gt;</code> label.<br>Content is saved to <code>%s</code> and served using PHP\'s <code>readfile()</code> function, so no PHP or shortcodes here.', 'machete' ) ), esc_html( MACHETE_RELATIVE_DATA_PATH . 'footer.html' ) ); ?></label></p>
-	<p>
+	<label for="footer_content"><p><?php printf( wp_kses_post( __( 'This code is included when the <code>wp_footer</code> action is called, normally just before the closing <code>&lt;/body&gt;</code> label.<br>Content is saved to <code>%s</code> and served using PHP\'s <code>readfile()</code> function, so no PHP or shortcodes here.', 'machete' ) ), esc_html( MACHETE_RELATIVE_DATA_PATH . 'footer.html' ) ); ?></p></label>
+	
 	<textarea name="footer_content" rows="8" cols="50" id="footer_content" class="large-text code"><?php echo esc_textarea( $machete_footer_content ); ?></textarea>
-	</p>
 	</fieldset>
 
 </div>
-
-
 
 <?php submit_button(); ?>
 
--- inc/utils/js/codemirror_tabs.js
+++ inc/utils/js/codemirror_tabs.js
@@ -1,60 +1,54 @@
-//wp.codeEditor.initialize( "header_content" );
-
 (function($, wp){
 
-	var existing_tabs = [];
+	var machete_utils = {
+		editor: {},
+		tabs: [],
+		current_tab: 'machete-tabs-tracking'
+	};
+
 	$('#machete-tabs a').each(function(){
-		existing_tabs.push( $(this).attr('data-tab') )
+		machete_utils.tabs[ $(this).attr('data-tab') ] = {};
 	});
-	console.log(existing_tabs);
-
-	current_tab = window.current_tab || 'machete-tabs-tracking';
 	
-	if ( window.location.hash && ( existing_tabs.indexOf( window.location.hash.substr(1) ) > -1 ) ) {
-		current_tab = window.location.hash.substr(1);
+	if ( window.location.hash && ( machete_utils.tabs[ window.location.hash.substr(1) ] ) ) {
+		machete_utils.current_tab = window.location.hash.substr(1);
 	}
-
-	var machete_editor;
 
 	updateTabs = function() {
 
 		$('#machete-tabs a').each(function(){
-			if ( current_tab == $(this).attr('data-tab')) {
+			if ( machete_utils.current_tab == $(this).attr('data-tab')) {
 				$(this).addClass('nav-tab-active');
 			} else {
 				$(this).removeClass('nav-tab-active');
 			}
 		});
 
-		$('.machete-tabs-content:not(#'+current_tab+')').hide();
-		$('#'+current_tab+'.machete-tabs-content').show();
+		$('.machete-tabs-content:not(#'+machete_utils.current_tab+')').hide();
+		$('#'+machete_utils.current_tab+'.machete-tabs-content').show();
 		// machete-tabs-header > header_content
-		textarea_id = current_tab.replace('machete-tabs-','') + '_content';
+		textarea_id = machete_utils.current_tab.replace('machete-tabs-','') + '_content';
 		//console.log( textarea_id );
-		if ( machete_editor && machete_editor.codemirror ){
-			machete_editor.codemirror.toTextArea();
+		if ( machete_utils.editor && machete_utils.editor.codemirror ){
+			machete_utils.editor.codemirror.toTextArea();
 		}
 		if (textarea_id != 'tracking_content'){
 			// la pestaña de analytics no tiene editor
-			machete_editor = wp.codeEditor.initialize( textarea_id );
+			machete_utils.editor = wp.codeEditor.initialize( textarea_id );
+			//machete_utils.editor.codemirror.doc.getValue();
 		}
-		
-		//console.log (machete_editor);
-		
 	}
-
 	updateTabs();
 
 	$('.machete-tabs-content h2').hide();
 
 	$('#machete-tabs').show().find('a').click( function ( e ) {
 		//e.preventDefault();
-		current_tab = $(this).attr('data-tab');
+		machete_utils.current_tab = $(this).attr('data-tab');
 
 		$('html, body').animate({
 			scrollTop: $('#machete-tabs').offset().top - 120
 		}, 500 );
-
 		//console.log( current_tab );
 		updateTabs();
 	});
