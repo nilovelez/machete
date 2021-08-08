a430f18c9d269abf9abcb8366162a5f6e558211d
Nilo Velez <nilvoelez@gmail.com>
2021-08-08 19:47:31 +0200

    Modified the cookie bar templates

--- inc/cookies/templates/cookies_bar_js.js
+++ inc/cookies/templates/cookies_bar_js.js
@@ -22,90 +22,118 @@ var machete_cookie_bar = (function(){
 		}
 		return null;
 	}
+
+	var my_createElement = function( tag, htmlid = null, htmlclass = null, html = null ){
+		element = document.createElement( tag );
+		if ( !! htmlid ) {
+			element.id = htmlid;
+		}
+		if ( !! htmlclass ) {
+			element.className = htmlclass;
+		}
+		if ( !! html ) {
+			element.innerHTML = html;
+		}
+		return element;
+	}
+
 	var get_status = function(){
 		return get_cookie( 'machete_accepted_cookies' ) || 'no';
 	}
 
-	var container = (function(){
-		var container       = document.createElement( 'div' );
-		container.id        = 'machete_cookie_container';
-		container.className = 'machete_cookie_container';
-		container.innerHTML = machete_cookies_bar_html;
-		Object.assign(container.style,{
-			position: 'fixed',
-			zIndex: 99999,
-			bottom: 0,
-			width: '100%'
-		});
-		var body = document.getElementsByTagName( 'body' )[0];
+	var cookiebar = (function(){
+		var cookiebar = my_createElement(
+			'div',
+			'machete_cookie_bar',
+			'machete_cookie_bar',
+			 machete_cookies_bar_html
+		);
+		cookiebar.querySelector("#machete_accept_cookie_btn").addEventListener(
+			'click', function(){
+					machete_cookie_bar.accept('yes');
+				} , false );
+		cookiebar.querySelector("#machete_accept_cookie_btn_partial").addEventListener(
+			'click', function(){
+					machete_cookie_bar.accept('partial');
+				} , false );
 		return {
 			add : function(){
-				body.appendChild( container );
-				document.getElementById( 'machete_accept_cookie_btn' ).addEventListener(
-					'click', machete_cookie_bar.accept, false );
+				document.getElementById('machete_cookie_container').appendChild( cookiebar );
 			},
 			remove : function(){
-				var container = document.getElementById('machete_cookie_container');
-				container.remove();
+				document.getElementById('machete_cookie_bar').remove();
 			}
 		}
 	})();
 
 	var configbar = (function(){
-		var configbar       = document.createElement( 'div' );
-		configbar.id        = 'machete_cookie_configbar';
-		configbar.className = 'machete_cookie_configbar';
-		configbar.innerHTML = machete_cookies_configbar_html;
-		Object.assign(configbar.style,{
-			position: 'fixed',
-			zIndex: 99999,
-			bottom: '10%'
-		});
-		var body = document.getElementsByTagName( 'body' )[0];
+		var configbar = my_createElement(
+			'div',
+			'machete_cookie_configbar',
+			'machete_cookie_configbar',
+			machete_cookies_configbar_html
+		);
+		configbar.querySelector("#machete_cookie_config_btn").addEventListener(
+			'click', function(){
+				machete_cookie_bar.config();
+			}, false );
+
 		return {
 			add : function(){
-				body.appendChild( configbar );
-				document.getElementById( 'machete_cookie_config_btn' ).addEventListener(
-					'click', machete_cookie_bar.config, false );
+				document.getElementById('machete_cookie_container').appendChild( configbar );
 			},
 			remove : function(){
-				var configbar = document.getElementById('machete_cookie_configbar');
-				configbar.remove();
+				document.getElementById('machete_cookie_configbar').remove();
 			}
 		}
 	})();
 
-	var body = document.getElementsByTagName( 'body' )[0];
-
 	return {
 		init: function(){
+
+			var container = my_createElement(
+				'div',
+				'machete_cookie_container',
+				'machete_cookie_container'
+			);
+			Object.assign(container.style,{
+				position: 'fixed',
+				zIndex: 99999,
+				bottom: 0,
+				width: '100%'
+			});
+			/*
+			var cookies_css = my_createElement(
+				'style',
+				null,
+				null,
+				machete_cookies_css
+			);
+			container.appendChild( cookies_css );
+			*/
+			var body = document.getElementsByTagName( 'body' )[0];
+			body.appendChild( container );
 			
 			if ( 'no' === get_status() ) {
-				container.add();	
+				cookiebar.add();	
 			} else {
 				configbar.add()
 			}
 		},
-		addCookieConfig:  function(){
-			body.appendChild( config );
-			document.getElementById( 'machete_cookie_config_btn' ).addEventListener( 'click', machete_cookie_bar.config, false );
-		},
 		accept: function( cookies = 'yes' ){
 			// saves the cookie settings an closes the cookie bar
 			set_cookie( 'machete_accepted_cookies', cookies, 365 );
-			container.remove();
+			cookiebar.remove();
 			configbar.add();
 		},
 		config: function(){
 			// reopens the cookie bar
 			configbar.remove();
-			container.add();
+			cookiebar.add();
 		},
 		status: function(){
 			return get_status();
 		}
-
-
 	}
 })();
 machete_cookie_bar.init();
