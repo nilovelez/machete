d2a34b51582e6e1eda17f09e0e4011949e774341
Nilo Velez <nilovelez@gmail.com>
2023-08-31 17:48:05 +0200

    cambios para revisar

--- inc/utils/frontend-functions.php
+++ inc/utils/frontend-functions.php
@@ -81,7 +81,5 @@ if ( ! function_exists( 'machete_custom_body_content' ) ) {
 	/**
 	 * Defines an empty function as fallback to prevent errors
 	 */
-	function machete_custom_body_content() {
-		return;
-	}
+	function machete_custom_body_content() {}
 }
